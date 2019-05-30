5f0e47117325de75c10e531214e9f2728174ca3a
hermasyp <hermas.yuda@gmail.com>
2019-05-30 23:52:46 +0700

    change title

--- avengerslist/lib/main.dart
+++ avengerslist/lib/main.dart
@@ -5,11 +5,10 @@ import 'avengerlist.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter OSG 4',
       theme: new ThemeData(primaryColor: Color.fromRGBO(58, 66, 86, 1.0)),
       home: new AvengerList(title: 'Avenger List'),
     );
